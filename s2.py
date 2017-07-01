68bff7981a8e812cba65f6596140c14f26934b84
Mrigesh Madaan <f2015756@pilani.bits-pilani.ac.in>
2017-07-01 23:27:54 +0530

    All 3d plots generated

--- s2.py
+++ s2.py
@@ -1,4 +1,5 @@
 import random
+from scipy.signal import hilbert
 import matplotlib
 matplotlib.use('TkAgg')
 import scipy.io
@@ -12,83 +13,208 @@ from matplotlib.backend_bases import key_press_handler
 from tkinter import *
 from matplotlib.figure import Figure
 from tkinter.filedialog import askopenfilename
+import plotly as py
+import plotly.graph_objs as go
+import pandas as pd
 
-mat = scipy.io.loadmat('./finalMat.mat')
-data = mat['finalMat']
+py.offline.init_notebook_mode()
+
+#mat = scipy.io.loadmat('./finalMat.mat')
+###########################################################################################
+
+###########################################################################################
+
 depths=[0 for x in range(193)]
-data2 = [[0 for x in range(192)] for y in range(1536)]
-for x in range(0,192):
-	for y in range(0,1536):
-		data2[y][x]=abs(data[y][x])
+#data2 = [[0 for x in range(192)] for y in range(1536)]
+#for x in range(0,192):
+#	for y in range(0,1536):
+#		data2[y][x]=abs(finalMat[y][x])
 global depthsArr
-#print (data[:,:])
-def init(self):
-    fig = plt.figure(figsize=(6, 10))
-    plt.rcParams["figure.figsize"] = 1,90
-    ax = fig.add_subplot(111)
-    ax.set_title('colorMap')
-    f=plt.imshow(data)
-    ax.set_aspect('equal')
 
-    cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
-    cax.get_xaxis().set_visible(False)
-    cax.get_yaxis().set_visible(False)
-    cax.patch.set_alpha(0.75)
-    cax.set_frame_on(False)
-    plt.colorbar(orientation='vertical')
-    plt.show()
+def generate2DPlot():
+	global filename
+	global finalMat
+	mat = scipy.io.loadmat(filename)
+	rawdata = mat['seatrial_data']
+
+	m = len(rawdata);
+	n = len(rawdata[0]);
+	#m contains number of rows, n contains the number of columns
+
+	############ Extract Useful data ###################
+	actualData = np.zeros((46080,192));
+	k=1
+	i=1
+	for j in range (0,1536):
+	    actualData[(j)*30+i-1:(j)*30+i+30-1-1,:]= rawdata[(j)*32+k+32-1:(j)*32+k+32-1-1-1+32-1,:];
+	mini = 9999;
+	maxi = -9999;
+
+	x = len(actualData);
+	y = len(actualData[0]);
+	for i in range(x):
+	    for j in range(y):
+	        if(mini > actualData[i,j]):
+	            mini = actualData[i,j]
+	        if(maxi < actualData[i,j]):
+	            mixi = actualData[i,j]
+	divider = max(abs(mini),maxi);
+	for i in range(x):
+	    for j in range(y):
+	        actualData[i,j] = actualData[i,j]/divider;
+	#print (actualData)
+	########### Normalization Ends #####################
+
+	########### Check if Along Map and across Map data. ######
+	summedData=np.zeros((1536,n));
+	finalMat = np.zeros((1536,n));
+	for i in range(0,n):
+	    for t in range(0,1536):
+	        for q in range(0,30):
+	            summedData[t,i] += (actualData[(t)*30+q,i]);       
+	    
+	#s = len(summedData[1])
+	#print (s)
+	print ("good to go")
+	for i in range(0,192):
+	    #h = abs(hilbert(summedData[:,i]))
+	    h = abs((summedData[:,i]))
+	    finalMat[:,i] =255 - (abs((((h[:]-min(h[:]))/(max(h[:])-min(h[:])))*255)))
+
+def insertMetalPlate():
+	global finalMat
+	for i in range(850,950):
+	    for j in range(80,120):
+	        finalMat[i,j] = random.randint(150,200)
+
+def plot2d():
+	global finalMat
+	fig = plt.figure(figsize=(6, 10))
+	plt.rcParams["figure.figsize"] = 1,90
+	ax = fig.add_subplot(111)
+	ax.set_title('colorMap')
+	plt.imshow(finalMat)
+	ax.set_aspect('equal')
+
+	cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+	cax.get_xaxis().set_visible(False)
+	cax.get_yaxis().set_visible(False)
+	cax.patch.set_alpha(0.75)
+	cax.set_frame_on(False)
+	plt.colorbar(orientation='vertical')
+	plt.show()
 
 def selectFileData():
-    global filename
-    filename=askopenfilename()
-    print(filename)
-    #Label(ctr_right,text=filename,width=15).grid(column=1, row=5)
-    entry_B.insert(END,filename)
+	global filename
+	filename=askopenfilename()
+	print(filename)
+	entry_B.insert(END,filename)
 
 def selectFileChirpData():
     global filename2
     filename2=askopenfilename()
     print(filename2)
-    #Label(ctr_right,text=filename,width=15).grid(column=1, row=5)
     entry_C.insert(END,filename2)
 
 def interpolate(arr2):
 	global depthsArr
 	n = 20;
-	depthsArr = np.zeros((n,192));
+	depthsArr = np.zeros((n,192))
 	for x in range(0,192):
-		if arr2[x]>7:
-			depthsArr[0,x]=arr2[x]
-		else:
-			depthsArr[0,x]=7
-
+		#if arr2[x]>7:
+		depthsArr[0,x]=-1*arr2[x]
+		#else:
+		#	depthsArr[0,x]=7
 	for i in range(1,n):
 	    for j in range(0,192):
 	        rand = random.randint(-10,11)
 	        depthsArr[i,j] = depthsArr[0,j] + (rand/50)
 
-	print (depthsArr)
-	return depthsArr
+	#print (depthsArr)
+	#return depthsArr
+
+def interpolateAndCreateObject(arr2):
+	global depthsArr
+	n = 20;
+	depthsArr = np.zeros((n,192));
+	for x in range(0,192):
+		#f arr2[x]>7:
+		depthsArr[0,x]=-1*arr2[x]
+		#else:
+		#	depthsArr[0,x]=7
+	for i in range(1,n):
+	    for j in range(0,192):
+	        rand = random.randint(-10,11)
+	        depthsArr[i,j] = depthsArr[0,j] + (rand/100)
+	for i in range(8,12):
+		for j in range(80,120):
+			depthsArr[i,j]+=1.5;
+	for i in range(9,11):
+		for j in range(90,110):
+			depthsArr[i,j]+=0.5
+	for i in range(10,11):
+		for j in range(10,110):
+			depthsArr[i,j]+=0.25
 
 def plot3d():
 	global depthsArr
 	fig=plt.figure()
 	ax=fig.gca(projection='3d')
 	X=np.arange(0,1920,10)
-	print(X)
+	#print(X)
 	Y=np.arange(0,200,10)
-	print(Y)
+	#print(Y)
 	X, Y = np.meshgrid(X, Y)
 	Z=depthsArr
-	surf=ax.plot_surface(X,Y,Z, cmap=cm.Blues, rstride=10,cstride=10,linewidth=0, antialiased=False)
+	surf=ax.plot_surface(X,Y,Z, cmap=cm.coolwarm, rstride=10,cstride=10,linewidth=0, antialiased=False)
 	ax.set_zlim(0, 8)
 	ax.zaxis.set_major_locator(LinearLocator(10))
 	ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+	ax.set_xlabel('X axis')
+	ax.set_ylabel('Y axis')
+	ax.set_zlabel('Z axis')
 	fig.colorbar(surf, shrink=0.5, aspect=10)
+	# #axes = Axes3D(fig)
+	# canvas=FigureCanvasTkAgg(fig,master=ctr_mid)
+	
+	# #map = Basemap()
+
+	# #axes.add_collection3d(map.drawcoastlines(linewidth = 0.3, color = "white"))
+	# #axes.add_collection3d(map.drawcountries(linewidth = 0.3, color = "white"))
+	# #axes.add_collection3d(map.drawstates(linewidth = 0.3, color = "white"))
+	# canvas.show()
+	# canvas.get_tk_widget().pack(side=TOP,fill=BOTH,expand=1)
 	plt.show()
 
-def getDepths(self):
+def plot3dByPlotly():
+	global depthsArr
+	data = [go.Surface(z=depthsArr)]
+	layout = go.Layout(scene = dict(
+		xaxis = dict(
+			nticks=18, 
+			range = [0,192],
+			),
+		yaxis = dict(
+			nticks=20, 
+			range = [0,20],
+			),
+		zaxis = dict(
+			nticks=10, 
+			range = [-10,10],
+			),
+		),
+	width=1000,
+	margin=dict(
+		r=20, l=10,b=10, t=10
+		)
+	)
+	fig = go.Figure(data=data,layout=layout)
+	py.offline.plot(fig)
+
+def getDepths():
 	i=0
+	global finalMat
+	data2=finalMat
 	for y in range(0,192):
 		runningAverage=0
 		runningAverage=0
@@ -102,6 +228,10 @@ def getDepths(self):
 				#print("depth")
 				#print(x)
 				depths[i]=x/133
+				if depths[i]>7.5:
+					depths[i]=7.5
+				elif depths[i]<6.5:
+					depths[i]=6.5
 				i=i+1
 				break
 		
@@ -115,43 +245,41 @@ def getDepths(self):
 		#print(y)
 		#print(" ")
 	#print(depths)
-	interpolate(depths)	
-	plot3d()
+	return depths
 
-def plot3d2():
-	fig = plt.figure()
-	ax = fig.gca(projection='3d')
+def simple2dPlot(self):
+	generate2DPlot()
+	plot2d()
 
-	# Make data.
-	X = np.arange(-5, 5, 0.25)
-	Y = np.arange(-5, 5, 0.25)
-	X, Y = np.meshgrid(X, Y)
-	R = np.sqrt(X**2 + Y**2)
-	Z = np.sin(R)
-	print(" Z ")
-	print(Z)
-	print(len(Z[0,:]))
-	# Plot the surface.
-	surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-	                       linewidth=0, antialiased=False)
+def Plot2DwithMetalPlate(self):
+	generate2DPlot()
+	insertMetalPlate()
+	plot2d()
 
-	# Customize the z axis.
-	ax.set_zlim(-1.01, 1.01)
-	ax.zaxis.set_major_locator(LinearLocator(10))
-	ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+def simple3dPlot(self):	
+	generate2DPlot()
+	interpolate(getDepths())	
+	plot3dByPlotly()
 
-	# Add a color bar which maps values to colors.
-	fig.colorbar(surf, shrink=0.5, aspect=5)
+def plot3dWithMetalStrip(self):
+	generate2DPlot()
+	insertMetalPlate()
+	interpolate(getDepths())	
+	plot3dByPlotly()
 
-	plt.show()
+def plot3dWithObject(self):
+	generate2DPlot()
+	interpolateAndCreateObject(getDepths())	
+	plot3dByPlotly()
+
 ########################################################################
 root = Tk()
 root.title('2D image generation for SONAR data')
 root.geometry('{}x{}'.format(800, 600))
 
 # create all of the main containers
-top_frame = Frame(root, bg='#ddd', width = 450, height=50, pady=3)
-center = Frame(root, bg='#ddd', width=350, height=190, padx=3, pady=3)
+top_frame = Frame(root, bg='white', width = 450, height=50, pady=10, padx=10, marginx=100)
+center = Frame(root, bg='#ddd', width=450, height=190, padx=3, pady=3)
 #btm_frame = Frame(root, bg='white', width = 450, height = 45, pady=3)
 #btm_frame2 = Frame(root, bg='lavender', width = 450, height = 60, pady=3)
 
@@ -165,6 +293,8 @@ center.grid(row=1, sticky="nsew")
 #btm_frame2.grid(row = 4, sticky="ew")
 
 # create the center widgets
+top_frame.grid_rowconfigure(1,weight=1)
+top_frame.grid_columnconfigure(1,weight=1)
 center.grid_rowconfigure(0, weight=1)
 center.grid_columnconfigure(1, weight=1)
 
@@ -181,13 +311,13 @@ width_label = Label(ctr_right,background='white', text = 'Rows: ',borderwidth = 
 length_label = Label(ctr_right,background='white', text = 'Columns:')
 height_label = Label(ctr_right,background='white', text = 'Start Point:')
 button_b1=Button(ctr_right,text="Generate Plot")
-button_b2=Button(ctr_right,text="Browse SONAR File", command=selectFileData)
-button_b3=Button(ctr_right,text="Browse Chirp File", command=selectFileChirpData)
+button_b2=Button(top_frame,text="Browse SONAR File", command=selectFileData)
+button_b3=Button(top_frame,text="Browse Chirp File", command=selectFileChirpData)
 entry_W = Entry(ctr_right, background="white")
 entry_L = Entry(ctr_right, background="white")
 entry_H = Entry(ctr_right, background="white")
-entry_B = Entry(ctr_right, background="white")
-entry_C = Entry(ctr_right,background="white")
+entry_B = Entry(top_frame, background="white")
+entry_C = Entry(top_frame,background="white")
 # use e.get() to get the string.
 #use int(e.get())to convert to int
 # use button_b1.bind('<BUTTON-1>',functionName)
@@ -198,13 +328,13 @@ width_label.grid(row = 1, column = 0,padx=10, pady=10)
 length_label.grid(row = 2, column = 0,padx=10, pady=10)
 height_label.grid(row = 3, column = 0,padx=10, pady=10)
 button_b1.grid(row = 4, column=0,padx=10, pady=10)
-button_b2.grid(row=5,column=0,padx=10,pady=10)
-button_b3.grid(row=5,column=1,padx=10,pady=10)
+button_b2.grid(row=0,column=0,padx=10,pady=10)
+button_b3.grid(row=1,column=0,padx=10,pady=10)
 entry_W.grid(row = 1, column = 1)
 entry_L.grid(row = 2, column = 1)
 entry_H.grid(row = 3, column = 1)
-entry_B.grid(row=8,column=0)
-entry_C.grid(row=9,column=0)
+entry_B.grid(row=0,column=1)
+entry_C.grid(row=1,column=1)
 #############################################################################
 def init2(self):
     f=Figure(figsize=(4,6),dpi=100)
@@ -220,5 +350,5 @@ def init2(self):
     canvas.get_tk_widget().pack(side=TOP,fill=BOTH,expand=1)
 #############################################################################
 ##############################################################################
-button_b1.bind('<Button-1>',getDepths)
+button_b1.bind('<Button-1>',simple3dPlot)
 root.mainloop()
